e9e39c6f4dc2b77c262b929b1b565fd16d899b26
Sebas973 <monterosebastian102@gmail.com>
2025-08-04 00:58:59 -0600

    Update Proyecto

--- ProyectoWeb/assets/js/portafolio.js
+++ ProyectoWeb/assets/js/portafolio.js
@@ -4,20 +4,23 @@ function cargarPortafolio() {
     $.getJSON(url, function (data) {
         $.each(data, function (index, posts) {
             const cardHTML = `
-            <div class="col">
-                <div class="card h-100 shadow-sm">
-                    <div class="card-body">
-                        <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/${posts.imageId}/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style="width:100%; margin:0 auto;"></blockquote>
-                    </div>
-                </div>
+        <div class="col">
+          <div class="card h-100 border-0 shadow-sm">
+            <div class="card-body p-0">
+              <blockquote class="instagram-media w-100"
+                data-instgrm-permalink="https://www.instagram.com/p/${posts.imageId}/?utm_source=ig_embed&amp;utm_campaign=loading"
+                data-instgrm-version="14"
+                style="margin:0 auto; border:0; padding:0;">
+              </blockquote>
             </div>
-            `;
+          </div>
+        </div>
+      `;
 
             $('#InstagramPostsContainer').append(cardHTML);
         })
+        window.instgrm.Embeds.process()
     });
 
-    if (window.instgrm) {
-        window.instgrm.Embeds.process();
-    }
+
 }
